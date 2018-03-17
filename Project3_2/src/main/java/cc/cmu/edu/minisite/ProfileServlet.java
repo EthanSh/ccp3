6774582b7f73a33dcbe2aa71fb04313f7e857c70
babyfat <babyfat@babyfatdeMBP.fios-router.home>
2018-03-16 21:56:10 -0400

    task 1

--- Project3_2/src/main/java/cc/cmu/edu/minisite/ProfileServlet.java
+++ Project3_2/src/main/java/cc/cmu/edu/minisite/ProfileServlet.java
@@ -12,17 +12,50 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import java.sql.*;
+
 public class ProfileServlet extends HttpServlet {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://ccp3.cwyuuwgo8ukt.us-east-1.rds.amazonaws.com/user";
+
+    //  Database credentials
+    static final String USER = "xingches";
+    static final String PASS = "1993vivid";
+
+    Connection conn = null;
+    Statement stmt = null;
 
     public ProfileServlet() {
         /*
             Your initialization code goes here
         */
+        super();
+
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) 
             throws ServletException, IOException {
+
+
+
         JSONObject result = new JSONObject();
         /*
             PreparedStatement leads to fast execution and helps prevent 
@@ -33,8 +66,38 @@ public class ProfileServlet extends HttpServlet {
         */
         String name = request.getParameter("id");
         String pwd = request.getParameter("pwd");
-        String query = "SELECT A FROM B WHERE C = ? AND D = ?";
+        String query = "SELECT * FROM username WHERE username=" + name;
+//        String query = "SELECT * FROM username WHERE username=" + name + " AND password="+ pwd;
 
+        try{
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            String username = rs.getString("username");
+            String profile = rs.getString("avatar");
+            result.put("name", username);
+            result.put("profile", profile);
+            //STEP 6: Clean-up environment
+            rs.close();
+//            stmt.close();
+//            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch (Exception exc){
+            exc.printStackTrace();
+        }finally {
+//            try{
+//                if(stmt!=null)
+//                    stmt.close();
+//            }catch(SQLException se2){
+//            }// nothing we can do
+//            try{
+//                if(conn!=null)
+//                    conn.close();
+//            }catch(SQLException se){
+//                se.printStackTrace();
+//            }//end finally try
+        }
 
 
         /*
